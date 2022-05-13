50f6a1036cc0b8db56174ebfa65814902b9c179c
Umesh Pedde <umesh.pedde@easebuzz.in>
2022-05-13 15:10:38 +0530

    This is initial commit for this project.

---
+++ MyCocoapod/MyCocoapod/MyCocoapod.h
@@ -0,0 +1,18 @@
+//
+//  MyCocoapod.h
+//  MyCocoapod
+//
+//  Created by umesh pedde on 13/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyCocoapod.
+FOUNDATION_EXPORT double MyCocoapodVersionNumber;
+
+//! Project version string for MyCocoapod.
+FOUNDATION_EXPORT const unsigned char MyCocoapodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyCocoapod/PublicHeader.h>
+
+
---
+++ MyCocoapod/MyCocoapod/TextfieldDemo.swift
@@ -0,0 +1,49 @@
+//
+//  TextfieldDemo.swift
+//  MyCocoapod
+//
+//  Created by umesh pedde on 13/05/22.
+//
+
+import Foundation
+import UIKit
+public class Service{
+    private init() {}
+    
+    public static func doSomething() -> String{
+        return "My pod is working....."
+    }
+}
+extension UITextField{
+    @IBInspectable var doneAccessory: Bool{
+        get{
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+
+        self.inputAccessoryView = doneToolbar
+    }
+
+    @objc func doneButtonAction()
+    {
+        self.resignFirstResponder()
+    }
+}
+
